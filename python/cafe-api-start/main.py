eb2ae9229f1a46bea8cad540d0714d9f84bdf2ac
kadew <kadewilliams96@gmail.com>
2021-09-09 13:21:26 -0500

    completed the cafe api project with documentation

--- python/cafe-api-start/main.py
+++ python/cafe-api-start/main.py
@@ -2,8 +2,11 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import random
 
+from werkzeug.utils import redirect
+
 app = Flask(__name__)
 
+param = { "apiKey": "TopSecretAPIKey"}
 
 ##Connect to Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
@@ -59,10 +62,54 @@ def search():
 
 
 ## HTTP POST - Create Record
+@app.route('/add', methods=["POST"])
+def add():
+    new_cafe = Cafe(
+        name=request.form.get('name'),
+        map_url=request.form.get('map_url'),
+        img_url=request.form.get('img_url'),
+        location=request.form.get('location'),
+        seats=request.form.get('seats'),
+        has_toilet=bool(request.form.get('has_toilet')),
+        has_wifi=bool(request.form.get('has_wifi')),
+        has_sockets=bool(request.form.get('has_sockets')),
+        can_take_calls=bool(request.form.get('can_take_calls')),
+        coffee_price=request.form.get('coffee_price'))
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success":"Successfully added the new cafe."})
 
 ## HTTP PUT/PATCH - Update Record
+@app.route('/update-price/<cafe_id>', methods=['PATCH'])
+def update(cafe_id):
+    try:
+        cafe = db.session.query(Cafe).get(cafe_id)
+        new_price = request.args.get('new_price')    
+
+        cafe.coffee_price = new_price
+
+        db.session.commit()
+
+        return jsonify({"success": "Successfully updated the price."}), 200
+    except Exception as e:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database"}), 404
 
 ## HTTP DELETE - Delete Record
+@app.route('/report-closed/<cafe_id>', methods=["DELETE"])
+def delete(cafe_id):
+    api_key = request.args.get('api-key')
+    if api_key != param['apiKey']:
+        return jsonify({"Unauthorized Access": "You do not have the correct permissions to delete elements from the database"}), 403
+    try:
+        cafe_to_delete = db.session.query(Cafe).get(cafe_id)
+        db.session.delete(cafe_to_delete)
+        db.session.commit()
+        return jsonify({"Success": "Cafe delete successful"}), 200
+    except Exception as e:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database"}), 404
+
+
+
 
 
 if __name__ == "__main__":
